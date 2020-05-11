891d2cea60488e1c09ef7c63edeacb6f8f76d0e9
timothywu52 <whitelion_vito@hotmail.com>
2020-05-11 23:48:41 +0800

    Initial Version

---
+++ src/main/kotlin/com/tom/Hello.kt
@@ -0,0 +1,13 @@
+package com.tom
+
+fun main(args: Array<String>) {
+//    println("hello kotlin")
+    val h = Human()
+    h.hello()
+}
+
+class Human{
+    fun hello(){
+        println("Hello kotlin")
+    }
+}
