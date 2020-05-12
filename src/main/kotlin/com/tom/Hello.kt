875882d29e818208ab6978157299019c02040657
timothywu52 <whitelion_vito@hotmail.com>
2020-05-12 23:50:46 +0800

    Kotlin variable data type

--- src/main/kotlin/com/tom/Hello.kt
+++ src/main/kotlin/com/tom/Hello.kt
@@ -4,6 +4,10 @@ fun main(args: Array<String>) {
 //    println("hello kotlin")
     val h = Human()
     h.hello()
+    var age : Int =25;
+    val weight = 65.5
+    val enroll : Boolean = true
+    println(age)
 }
 
 class Human{
